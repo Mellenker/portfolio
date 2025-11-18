969974e1571eb3031a10e924aab311772dc2a11b
Mellenker <melker.stafverfeldt@outlook.com>
2025-11-18 18:45:03 +0100

    More work on card styles and global text style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,21 @@
-import "./Card.css"
+import "./Card.css";
 
 function Card({ title, imgLink, description, link }) {
   return (
     <div className="card-container">
-      <img 
-        src={imgLink} 
-        alt = "Card Image"
-        className = "card-img"
-      />
-      <h3 className="card-title">{title}</h3>
-      <p className="card-desciption">{description}</p>
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        View Project
-      </a>
+      <img src={imgLink} alt="Card Image" className="card-img" />
+      <div className="card-info-container">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="card-btn"
+        >
+          View Project
+        </a>
+      </div>
     </div>
   );
 }
