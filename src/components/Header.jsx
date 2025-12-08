1c752b2bb3e7d7e1002e02f964a046222b63b01f
Mellenker <melker.stafverfeldt@outlook.com>
2025-12-08 22:44:28 +0100

    Added social media links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 // import { useState } from "react";
 // import { FaHambuger } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./Header.css";
 
 function Header({ name }) {
@@ -16,9 +17,26 @@ function Header({ name }) {
         {/* <button classname="hamburger" onclick={toggleMenu}></button> */}
 
         <nav className="header-navbar">
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
+          <a className="navbar-link" href="#about">
+            About
+          </a>
+          <a className="navbar-link" href="#projects">
+            Projects
+          </a>
+          <a className="navbar-link" href="#contact">
+            Contact
+          </a>
+          <div className="socialMedia-container">
+            <a
+              className="socialMediaLink"
+              href="https://www.linkedin.com/in/melker-stafverfeldt-458406363/"
+            >
+              <FaLinkedin />
+            </a>
+            <a className="socialMediaLink" href="https://github.com/Mellenker">
+              <FaGithub />
+            </a>
+          </div>
         </nav>
       </header>
     </div>
