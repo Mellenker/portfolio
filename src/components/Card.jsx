1550c6d2acef0e334cfc784056d7e4bb35e6e8ba
Mellenker <melker.stafverfeldt@outlook.com>
2025-12-18 14:18:58 +0100

    Improved responsiveness further

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -26,50 +26,54 @@ function Card({
   return (
     <div className="card-container">
       <img src={imgLink} alt="Card Image" className="card-img" />
-      <h3 className="card-title">{title}</h3>
-      <p className="card-description">{description}</p>
-      <div className="card-technologies-container">
-        {technologies &&
-          technologies.map((tech, index) => {
-            const iconName = TECH_ICONS[tech];
+      <div className="card-info">
+        <h3 className="card-info-title">{title}</h3>
+        <p className="card-info-description">{description}</p>
+        <div className="card-info-bottom-container">
+          <div className="card-tech-container">
+            {technologies &&
+              technologies.map((tech, index) => {
+                const iconName = TECH_ICONS[tech];
 
-            return (
-              <span key={index} className="tech-badge" data-tech={tech}>
-                {iconName ? (
-                  <img
-                    src={`https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${iconName}/${iconName}-original.svg`}
-                    className="tech-img"
-                    alt={tech}
-                  />
-                ) : (
-                  <span className="tech-alt">{tech}</span>
-                )}
-              </span>
-            );
-          })}
-      </div>
-      <div className="card-link-container">
-        {githubLink && (
-          <a
-            href={githubLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="button-1 button-1--github"
-          >
-            <FaGithub />
-            {/* View on GitHub */}
-          </a>
-        )}
-        {downloadLink && (
-          <a
-            href={downloadLink}
-            download
-            className="button-1 button-1--download"
-          >
-            <FaDownload />
-            {/* Download ZIP */}
-          </a>
-        )}
+                return (
+                  <span key={index} className="tech-badge" data-tech={tech}>
+                    {iconName ? (
+                      <img
+                        src={`https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${iconName}/${iconName}-original.svg`}
+                        className="tech-img"
+                        alt={tech}
+                      />
+                    ) : (
+                      <span className="tech-alt">{tech}</span>
+                    )}
+                  </span>
+                );
+              })}
+          </div>
+          <div className="card-link-container">
+            {githubLink && (
+              <a
+                href={githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button-1 button-1--github"
+              >
+                <FaGithub />
+                {/* View on GitHub */}
+              </a>
+            )}
+            {downloadLink && (
+              <a
+                href={downloadLink}
+                download
+                className="button-1 button-1--download"
+              >
+                <FaDownload />
+                {/* Download ZIP */}
+              </a>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
