fd5614c4aed644565ea87b15d124070fdcbdedf9
Mellenker <melker.stafverfeldt@outlook.com>
2026-01-19 17:40:30 +0100

    Added mailto button

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,10 +1,16 @@
+import "./Contact.css";
+import "./Button.css";
+
 function Contact() {
   const email = "melker.stafverfeldt@hotmail.com";
 
   return (
     <section id="contact" className="contact-section">
-      <h2>Kontakt</h2>
+      <h2 className="contact-heading">Kontakt</h2>
       <p>Om du vill kontakta mig kan du mejla mig på {email}</p>
+      <a href="mailto:melker.stafverfeldt@hotmail.com" className="button-1">
+        Mejla mig
+      </a>
     </section>
   );
 }
